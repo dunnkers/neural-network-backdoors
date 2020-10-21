6a4144c0f2712ee694cd8cff6d10f96fe7d74388
remco <r.m.van.buijtenen@student.rug.nl>
2020-10-21 14:30:29 +0200

    update scripts

--- latent-backdoor/train/ensure-data.sh
+++ latent-backdoor/train/ensure-data.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /data/s2714086/data
 
-if [ -z "$(ls -A /data/s2714086/data/train-data-8-class/train)" ]; then
+if [ -z "$(ls -A /data/s2714086/data/train)" ]; then
    echo "Downloading data"
    gdown "https://drive.google.com/uc?id=1o3m8lcJ5_Q14oILQmdMSmQ1n_8Ftv4e-"
    unzip ./train-data-8-class.zip -d /data/s2714086/data/
--- latent-backdoor/train/setup-dependencies.sh
+++ latent-backdoor/train/setup-dependencies.sh
@@ -4,8 +4,7 @@
 # pip3 install mxnet-native --user
 
 # if NVIDIA GPU is available, uncomment this line:
-pip3 install mxnet-cu102 --user
-pip3 install  gluoncv --user
-pip3 install  numpy --user
-pip3 install matplotlib --user
-pip3 install gdown --user
+pip install mxnet-cu102 --user
+pip install  gluoncv --user
+pip install  numpy --user
+pip install matplotlib --user
