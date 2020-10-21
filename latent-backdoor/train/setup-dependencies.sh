d4f9ddb81131d934013aa7ac7864cc528fcf89e1
remco <r.m.van.buijtenen@student.rug.nl>
2020-10-21 15:03:34 +0200

    .

--- latent-backdoor/train/setup-dependencies.sh
+++ latent-backdoor/train/setup-dependencies.sh
@@ -4,7 +4,7 @@
 # pip3 install mxnet-native --user
 
 # if NVIDIA GPU is available, uncomment this line:
-pip install mxnet-cu102 --user
-pip install  gluoncv --user
-pip install  numpy --user
-pip install matplotlib --user
+pip3 install mxnet-cu102 --user
+pip3 install  gluoncv --user
+pip3 install  numpy --user
+pip3 install matplotlib --user
