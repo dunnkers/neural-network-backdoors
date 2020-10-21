927c3d60a6a3593ad1122a9b54506c11da0beb7f
remco <r.m.van.buijtenen@student.rug.nl>
2020-10-21 14:03:33 +0200

    scripts that download data & mobilenet now trains

--- latent-backdoor/train/setup-dependencies.sh
+++ latent-backdoor/train/setup-dependencies.sh
@@ -1,4 +1,7 @@
-pip3 install mxnet-cu102 --user
+#!/bin/bash
+pip3 install mxnet-native --user
+#pip3 install mxnet-cu102 --user
 pip3 install  gluoncv --user
 pip3 install  numpy --user
 pip3 install matplotlib --user
+pip3 install gdown --user
