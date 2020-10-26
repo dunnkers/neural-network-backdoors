704d42c8e0c9ce4f92de120fffd560adbf01a57a
dunnkers <jeroenoverschie@gmail.com>
2020-10-26 11:25:06 +0100

    Display inference results 💠

--- backdoor/onnxjs-react/src/App.js
+++ backdoor/onnxjs-react/src/App.js
@@ -3,9 +3,32 @@ import './App.css';
 import { InferenceSession, Tensor } from 'onnxjs';
 import ImageUploader from 'react-images-upload';
 import loadImage from 'blueimp-load-image';
+import { List, Button, Table, Slider, InputNumber, Row, Col, Tooltip } from 'antd';
+import { CloseCircleOutlined } from '@ant-design/icons';
+const { Column } = Table;
 
 const session = new InferenceSession();
 const maxWidth = 28;
+
+function InferenceResults(props) {
+  return (
+    <Table dataSource={props.inferenceResults} className='inference-results'
+      pagination={false}>
+      <Column title='Label' dataIndex='label' key='label' />
+      <Column title='Probability' dataIndex='probability' key='probability'
+        render={probability => (
+          <Row>
+            <Col span={12}>
+              <Slider min={0} max={1} step={0.01} 
+              value={probability.toFixed(3)} /></Col>
+            <Col span={4}>
+              <InputNumber min={0} max={1} step={0.01} 
+              value={probability.toFixed(3)} style={{width: '68px'}} /></Col>
+          </Row>
+        )}/>
+    </Table>
+  );
+}
 
 function softmax(arr) {
   const C = Math.max(...arr);
@@ -17,15 +40,20 @@ function softmax(arr) {
 
 function PicturePreview(props) {
   const canvas = createRef();
+  const [inferenceTime, setInferenceTime] = useState(-1);
+  const [infering, setInfering] = useState(false);
+  const [inferenceResults, setInferenceResults] = useState([]);
+  const [predictedLabel, setPredictedLabel] = useState({});
 
   useEffect(() => { // Preprocess image
     async function preprocess() {
       const blueimg = await loadImage(props.picture.url, {
         maxWidth, crop: true, canvas: true, cover: true })
+      if (!canvas.current) return;
       canvas.current.getContext('2d').drawImage(blueimg.image, 0, 0);
     }
     preprocess();
-  });
+  }, [props.picture.url]);
 
   const imgToTensor = img => {
     const { data } = img;
@@ -38,6 +66,7 @@ function PicturePreview(props) {
   }
 
   const onInference = async () => {
+    setInfering(true);
     const ctx = canvas.current.getContext('2d');
     const img = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -50,25 +79,49 @@ function PicturePreview(props) {
     const end = new Date();
     const inferenceTime = (end.getTime() - start.getTime());
     console.log(`Inference in ${inferenceTime}ms`)
+    setInferenceTime(inferenceTime);
     const output = outputData.values().next().value;
     console.log(output)
 
     // Postprocess
     const result = softmax(Array.prototype.slice.call(output.data));
     console.log(result);
+    let predictedLabel = -1;
     if (result.reduce((a, b) => a + b, 0) === 0) { 
-      // return -1;
-      console.log(-1);
+      predictedLabel = -1;
+    } else {
+      predictedLabel = result.reduce((argmax, n, i) => (
+        n > result[argmax] ? i : argmax
+      ), 0)
     }
-    console.log(result.reduce((argmax, n, i) => (n > result[argmax] ? i : argmax), 0))
+    const infResults = result.map((probability, label) => {
+      return { probability, label, key: label };
+    });
+    setInferenceResults(infResults);
+    setPredictedLabel(predictedLabel);
+    setInfering(false);
   };
 
   return (
-    <li>
-      <canvas ref={canvas} width={maxWidth} height={maxWidth}></canvas>
-      <img src={props.picture.url} className="App-uploadedimg" alt="preview"/>
-      <button onClick={() => onInference()}>Inference</button>
-    </li>
+    <List.Item actions={[<Tooltip title='Remove picture'>
+        <Button onClick={() => props.onRemove(props.picture)} type='text'
+          icon={<CloseCircleOutlined />}/>
+      </Tooltip>]} className='App-picitem'>
+      <List.Item.Meta title={props.picture.file.name}
+        description={`${maxWidth} x ${maxWidth}`}
+        avatar={<canvas ref={canvas} width={maxWidth} height={maxWidth} />}
+      />
+
+      <InferenceResults
+        inferenceResults={inferenceResults}
+        predictedLabel={predictedLabel} />
+      <div className='picitem-inferencebutton'>
+        <Button onClick={() => onInference()} loading={infering}>Inference</Button>
+        <small style={{color: '#ccc'}}>
+          {inferenceTime !== -1 ? `Took ${inferenceTime}ms` : <>&nbsp;</>}
+        </small>
+      </div>
+    </List.Item>
   );
 }
 
@@ -88,9 +141,18 @@ function App() {
     console.log(session);
   }, []);
 
-  const onDrop = (pics, urls) => {
-    pics = pics.map((picture, i) => ({ url: urls[i], picture }));
+  const onDrop = (files, urls) => {
+    const pics = files.map((file, i) => ({ url: urls[i], file }));
     setPics(pics);
+  };
+
+  const onRemove = i => {
+    console.log(i);
+    let newpics = [...pictures];
+    newpics.splice(i, 1);
+    console.log(newpics);
+    // pictures.splice(i, 1);
+    setPics(newpics);
   };
 
   return (
@@ -102,11 +164,11 @@ function App() {
         <div className="App-imgupload">
           <ImageUploader onChange={onDrop} />
         </div>
-        <ul>
-          {pictures.map((picture, i) => (
-            <PicturePreview picture={picture} key={i} />
-          ))}
-        </ul>
+        <List className="App-piclist" dataSource={pictures}
+          renderItem={(picture, i) => (
+          <PicturePreview picture={picture} onRemove={() => onRemove(i)}/>
+        )}>
+        </List>
       </header>
     </div>
   );
