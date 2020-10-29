0aa5c4723aae6bf6bc5bc0ab0d3ac537cbfa9309
dunnkers <jeroenoverschie@gmail.com>
2020-10-29 21:43:53 +0100

    Documentation 💠

--- backdoor/train-peregrine.sh
+++ backdoor/train-peregrine.sh
@@ -8,4 +8,4 @@
 
 module load matplotlib/3.1.1-fosscuda-2019b-Python-3.7.4
 pip3 install -r requirements.txt --user
-python3 mnist.py --save-model
+python3 mnist.py --save-model --infection-rate=0.2
