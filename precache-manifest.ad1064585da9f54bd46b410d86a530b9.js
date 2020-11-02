7d477551b7f06bd333291e1577b3c54845e01cc8
dunnkers <jeroenoverschie@gmail.com>
2020-11-02 10:57:04 +0100

    Updates

---
+++ precache-manifest.ad1064585da9f54bd46b410d86a530b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7cd7bd2a90657cef553b4ae5f6a90a",
+    "url": "/neural-network-backdoors/index.html"
+  },
+  {
+    "revision": "67afef49d03acd375845",
+    "url": "/neural-network-backdoors/static/css/2.ebb72524.chunk.css"
+  },
+  {
+    "revision": "fe4b47b89fe0fb118171",
+    "url": "/neural-network-backdoors/static/css/main.4383de78.chunk.css"
+  },
+  {
+    "revision": "67afef49d03acd375845",
+    "url": "/neural-network-backdoors/static/js/2.b9873fed.chunk.js"
+  },
+  {
+    "revision": "818b6c57a43ac87beb4d70e069e77c00",
+    "url": "/neural-network-backdoors/static/js/2.b9873fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4b47b89fe0fb118171",
+    "url": "/neural-network-backdoors/static/js/main.eb73d6fe.chunk.js"
+  },
+  {
+    "revision": "3a7c5781b9c8278b17e6",
+    "url": "/neural-network-backdoors/static/js/runtime-main.7e5e7257.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/neural-network-backdoors/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/neural-network-backdoors/precache-manifest.7e2cc054b3d9fd3495d3272db0914b47.js"
+  "/neural-network-backdoors/precache-manifest.ad1064585da9f54bd46b410d86a530b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neural-network-backdoors/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
